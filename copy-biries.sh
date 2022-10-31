990c7c7681a6a85e8bf55017382a768bbb8a9914
hsinhoyeh <yhh92u@gmail.com>
2022-10-31 13:43:52 +0800

    Bump version v1.2.8

--- copy-biries.sh
+++ copy-biries.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export TAG="v1.2.6"
+export TAG="v1.2.8"
 docker pull footprintai/grandturks-smartcity-client:$TAG
 docker create -it --name smartcityclient footprintai/grandturks-smartcity-client:$TAG /bin/bash
 docker cp smartcityclient:/out/. release
