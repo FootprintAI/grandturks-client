2079dd63983bcc0089ec7aa6767451c1d82d245b
hsinhoyeh <yhh92u@gmail.com>
2024-05-20 11:23:22 +0800

    bump version to v2.2.1
    issue: #4

--- copy-biries.sh
+++ copy-biries.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export TAG="v2.0.2"
+export TAG="v2.2.1"
 docker pull footprintai/grandturks-kafeido-client:$TAG
 docker create -it --name kafeidoclient footprintai/grandturks-kafeido-client:$TAG /bin/bash
 docker cp kafeidoclient:/out/. kafeido
